5ff6c2b3d08de1dfa3af47e836e27b1b923fc615
tria.luism <spcbrasil.2021>
2021-09-13 15:40:45 -0300

    Aula 02

---
+++ src/com/filipemota/estrturadados/teste/Aula02.java
@@ -0,0 +1,12 @@
+package com.filipemota.estrturadados.teste;
+
+import com.filipemota.estrturadados.vetor.Vetor;
+
+public class Aula02 {
+
+    public static void main(String[] args){
+        Vetor vetor = new Vetor(5);
+        int i = 0;
+        i++;
+    }
+}
---
+++ src/com/filipemota/estrturadados/vetor/Vetor.java
@@ -0,0 +1,10 @@
+package com.filipemota.estrturadados.vetor;
+
+public class Vetor {
+
+    private String[] elementos;
+
+    public Vetor(int capacidade){
+        this.elementos = new String[capacidade];
+    }
+}
