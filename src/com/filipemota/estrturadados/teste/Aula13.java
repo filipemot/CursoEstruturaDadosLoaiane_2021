14b6d1fd30f2e599d2da3e3608149db0876f4582
tria.luism <spcbrasil.2021>
2021-10-07 15:01:59 -0300

    Aula 13 - Adjuste no teste

--- src/com/filipemota/estrturadados/teste/Aula13.java
+++ src/com/filipemota/estrturadados/teste/Aula13.java
@@ -2,6 +2,8 @@ package com.filipemota.estrturadados.teste;
 
 import com.filipemota.estrturadados.vetor.Lista;
 
+import java.util.ArrayList;
+
 public class Aula13 {
 
     public static void main(String[] args){
@@ -15,6 +17,13 @@ public class Aula13 {
             System.out.println(vetor.contem(4));
             System.out.println(vetor.contem(8));
 
+            //contem semelhante contains
+            ArrayList<Integer> lista = new ArrayList<>();
+            lista.add(3);
+            lista.add(4);
+            lista.add(5);
+            System.out.println(lista.contains(4));
+            System.out.println(lista.contains(8));
 
         } catch (Exception e) {
             e.printStackTrace();
